96f11a5870c91331b77581a43354714035568052
June Ang <June Ang>
2020-09-08 18:29:26 +0800

    Add goal list feature

---
+++ src/components/GoalList.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { goalRef } from "../firebase";
+
+class GoalList extends Component {
+  componentDidMount() {
+    goalRef.on("value", (snap) => {
+      let goals = [];
+      snap.forEach((goal) => {
+        const { email, title } = goal.val();
+        goals.push({ email, title });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2></h2>
+      </div>
+    );
+  }
+}
+
+export default GoalList;
