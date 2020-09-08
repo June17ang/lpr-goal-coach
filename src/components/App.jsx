bd39b92c91de8a2b4da669e829dad4c24c463759
June Ang <June Ang>
2020-09-08 18:11:22 +0800

    Add reducer and log user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { firebaseApp } from "../firebase";
 
 class App extends Component {
+  signOut = () => {
+    firebaseApp.auth().signOut();
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <h3>Goals</h3>
+        <div>Add Goal</div>
+        <div>Goal Lists</div>
+        <button className="btn btn-danger" onClick={this.signOut}>
+          Sign Out
+        </button>
+      </div>
+    );
   }
+}
+
+function mapStateToProps(state) {
+  return {};
 }
 
 export default App;
