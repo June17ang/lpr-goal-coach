45d2edbf1261d16b1f6be60f414ef1c1e01d670f
June Ang <June Ang>
2020-09-08 19:22:33 +0800

    Add complete goal list and clear all complete list

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ import { connect } from "react-redux";
 
 export const SIGNED_IN = "SIGNED_IN";
 export const SET_GOALS = "SET_GOALS";
+export const SET_COMPLETED = "SET_COMPLETED";
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineRecuders } from "redux";
 import user from "./reducer_user";
 import goals from "./reducer_goals";
+import completeGoals from "./reducer_complete_goals";
 
 export default combineRecuders({
   user,
   goals,
+  completeGoals,
 });
